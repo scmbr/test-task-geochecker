2e23351de98e962f493a4cb5e0ccd14b29c6a564
scmbr <romvasily@mail.ru>
2025-12-30 23:15:33 +0900

    feat: add repo interfaces

---
+++ internal/repository/interfaces.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"github.com/scmbr/test-task-geochecker/internal/domain/models"
+	"gorm.io/gorm"
+)
+
+type IncidentRepository interface {
+	Create(ctx context.Context, incident models.Incident) error
+	GetAll(ctx context.Context, offset, limit int) ([]models.Incident, error)
+	GetById(ctx context.Context, id string) (*models.Incident, error)
+	Update(ctx context.Context, id string, incident models.Incident) error
+	Delete(ctx context.Context, id string) error
+	CountUniqueUsers(ctx context.Context, incidentID string, since time.Time) (int, error)
+}
+type CheckRepository interface {
+	Create(ctx context.Context, check models.Check) error
+	GetById(ctx context.Context, id string) (*models.Check, error)
+}
+type Repository struct {
+	Incident IncidentRepository
+	Check    CheckRepository
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		Incident: NewIncidentRepository(db),
+		Check:    NewCheckRepository(db),
+	}
+}
