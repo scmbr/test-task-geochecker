4002a790101edec84cad28e0fc9f28996dd952eb
scmbr <romvasily@mail.ru>
2026-01-06 21:44:07 +0900

    feat: add errors for operator service

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -7,4 +7,7 @@ var (
 	ErrIncidentAlreadyExists = errors.New("incident already exists")
 	ErrCheckNotFound         = errors.New("check not found")
 	ErrCheckAlreadyExists    = errors.New("check already exists")
+	ErrOperatorNotFound      = errors.New("operator not found")
+	ErrOperatorAlreadyExists = errors.New("operator already exists")
+	ErrInvalidAPIKey         = errors.New("invalid api key")
 )
