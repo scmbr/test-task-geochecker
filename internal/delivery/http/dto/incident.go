85d7246a067a32be96dfcb7c7b890342a45b455e
scmbr <romvasily@mail.ru>
2026-01-09 21:36:21 +0900

    feat: add incident handler dto

---
+++ internal/delivery/http/dto/incident.go
@@ -0,0 +1,32 @@
+package dto
+
+import "time"
+
+type CreateIncidentRequest struct {
+	Latitude  float64 `json:"latitude" binding:"required"`
+	Longitude float64 `json:"longitude" binding:"required"`
+	Radius    uint8   `json:"radius" binding:"required"`
+}
+
+type GetAllIncidentsRequest struct {
+	Offset int `json:"offset" binding:"required"`
+	Limit  int `json:"limit" binding:"required"`
+}
+type GetAllIncidentsResponse struct {
+	Total     int32                 `json:"total"`
+	Incidents []GetIncidentResponse `json:"incidents"`
+}
+type GetIncidentResponse struct {
+	ID         string    `json:"id"`
+	OperatorID string    `json:"operator_id"`
+	Latitude   float64   `json:"latitude"`
+	Longitude  float64   `json:"longitude"`
+	Radius     uint8     `json:"radius"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+type UpdateIncidentRequest struct {
+	OperatorID *string  `json:"operator_id"`
+	Latitude   *float64 `json:"latitude"`
+	Longitude  *float64 `json:"longitude" `
+	Radius     *uint8   `json:"radius"`
+}
