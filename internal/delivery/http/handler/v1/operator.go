6087077baee6e7140e877924d44fb67a782b1bc2
scmbr <romvasily@mail.ru>
2026-01-09 22:04:22 +0900

    feat: add operator routes and handlers

---
+++ internal/delivery/http/handler/v1/operator.go
@@ -0,0 +1,56 @@
+package v1
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	handler_dto "github.com/scmbr/test-task-geochecker/internal/delivery/http/dto"
+	"github.com/scmbr/test-task-geochecker/internal/service"
+	service_dto "github.com/scmbr/test-task-geochecker/internal/service/dto"
+)
+
+func (h *Handler) initOperatorRoutes(api *gin.RouterGroup) {
+	operators := api.Group("/operators", h.operatorIdentity)
+	{
+		operators.POST("", h.createOperator)
+		operators.DELETE("/:id", h.revokeOperator)
+	}
+}
+func (h *Handler) createOperator(c *gin.Context) {
+	var input handler_dto.CreateOperatorRequest
+	if err := c.ShouldBindJSON(&input); err != nil {
+		newResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	if err := h.service.Operator.Create(c.Request.Context(), &service_dto.CreateOperatorInput{
+		Name:   input.Name,
+		APIKey: input.APIKey,
+	}); err != nil {
+		newResponse(c, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+	c.Status(http.StatusCreated)
+}
+func (h *Handler) revokeOperator(c *gin.Context) {
+	id := c.Param("id")
+	callerID := c.GetString(operatorCtx)
+	if callerID == id {
+		newResponse(c, http.StatusForbidden, "cannot revoke yourself")
+		return
+	}
+	if _, err := uuid.Parse(id); err != nil {
+		newResponse(c, http.StatusBadRequest, "invalid id format")
+		return
+	}
+	if err := h.service.Operator.Revoke(c.Request.Context(), id); err != nil {
+		if errors.Is(err, service.ErrOperatorNotFound) {
+			newResponse(c, http.StatusNotFound, "operator not found")
+			return
+		}
+		newResponse(c, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+	c.Status(http.StatusNoContent)
+}
