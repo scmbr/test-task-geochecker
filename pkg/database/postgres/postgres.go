9f778fec5466005b3477866c2a5bc3a163498554
scmbr <romvasily@mail.ru>
2026-01-10 17:49:44 +0900

    feat: add function for connection to postgres db

---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(cfg Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
