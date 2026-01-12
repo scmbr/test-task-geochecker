438e99287e8bafc45d12ca3b99df8ac738d27250
scmbr <romvasily@mail.ru>
2026-01-12 19:42:48 +0900

    fix: fix check model Use geometry type for lat and lon

--- internal/domain/models/check.go
+++ internal/domain/models/check.go
@@ -5,7 +5,6 @@ import "time"
 type Check struct {
 	CheckID   string    `gorm:"primaryKey;column:check_id"`
 	UserID    string    `gorm:"column:user_id;not null"`
-	Longitude float64   `gorm:"column:longitude;not null"`
-	Latitude  float64   `gorm:"column:latitude;not null"`
+	Location  string    `gorm:"type:geometry(POINT,4326);column:location;not null"`
 	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
 }
