d7da11b4984b6926cd4f18cf10e3cada5a598fb1
scmbr <romvasily@mail.ru>
2026-01-12 19:43:13 +0900

    fix: fix incident model Use geometry type for lat and lon

--- internal/domain/models/incident.go
+++ internal/domain/models/incident.go
@@ -5,9 +5,8 @@ import "time"
 type Incident struct {
 	IncidentID string     `gorm:"primaryKey;column:incident_id"`
 	OperatorID string     `gorm:"column:operator_id;not null"`
-	Longitude  float64    `gorm:"column:longitude;not null"`
-	Latitude   float64    `gorm:"column:latitude;not null"`
-	Radius     uint8      `gorm:"column:radius;not null"`
+	Location   string     `gorm:"type:geometry(POINT,4326);column:location;not null"`
+	Radius     uint32     `gorm:"column:radius;not null"`
 	CreatedAt  time.Time  `gorm:"column:created_at;autoCreateTime"`
 	DeletedAt  *time.Time `gorm:"column:deleted_at;default:null"`
 	UpdatedAt  *time.Time `gorm:"column:updated_at;default:null"`
@@ -16,5 +15,5 @@ type UpdateIncidentInput struct {
 	OperatorID *string  `json:"operator_id"`
 	Latitude   *float64 `json:"latitude"`
 	Longitude  *float64 `json:"longitude"`
-	Radius     *uint8   `json:"radius"`
+	Radius     *uint32  `json:"radius"`
 }
