df8ed576d4270e3b169572c37a0db300d694b712
scmbr <romvasily@mail.ru>
2026-01-12 19:40:21 +0900

    fix: fix check and incident migrations Use geometry type for lat and lon

--- migrations/20251228133553_create_checks_table.up.sql
+++ migrations/20251228133553_create_checks_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE checks (
     check_id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
-    latitude NUMERIC(8,6) NOT NULL,
-    longitude NUMERIC(9,6) NOT NULL,
+    location geometry(POINT,4326) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     deleted_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ
 );
+
+CREATE INDEX idx_incidents_location ON incidents USING GIST(location);
