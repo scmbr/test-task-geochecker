a3fea02614d846aa3b175e403461b252db886a2c
scmbr <romvasily@mail.ru>
2026-01-15 22:04:33 +0900

    fix: fix CreateCheckRequest dto

--- internal/delivery/http/dto/check.go
+++ internal/delivery/http/dto/check.go
@@ -2,8 +2,8 @@ package dto
 
 type CreateCheckRequest struct {
 	UserID    string  `json:"user_id" binding:"required"`
-	Latitude  float64 `json:"latitude_id" binding:"required"`
-	Longitude float64 `json:"longitude_id" binding:"required"`
+	Latitude  float64 `json:"latitude" binding:"required"`
+	Longitude float64 `json:"longitude" binding:"required"`
 }
 type CreateCheckResponse struct {
 	Count     uint16                `json:"count" binding:"required"`
